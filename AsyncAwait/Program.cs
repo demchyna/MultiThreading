2c714e6a5210e1878b92ded1637e4be0dc6a07e1
mdem <mdem@softserveinc.com>
2018-03-27 11:01:06 +0300

    27.03.2018

---
+++ AsyncAwait/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAwait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+            app.AsyncAwaitMethod();
+
+            Thread.CurrentThread.Name = "Main Thread";
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Thread.Sleep(500);
+            }
+
+            Console.ReadKey();
+        }
+
+        public async void AsyncAwaitMethod()
+        {
+            string message =  await DoSomethingAsync(10);
+            Console.WriteLine(message);
+        }
+
+
+        public async static Task<string> DoSomethingAsync(int count)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "My Thread";
+                //Thread.CurrentThread.IsBackground = false;
+
+                int counter = 0;
+                for (; counter < count; counter++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name);
+                    Thread.Sleep(1000);
+                }
+
+                return string.Format("{0} was executed {1} times", Thread.CurrentThread.Name, counter);
+            });
+        }
+    }
+}
---
+++ Tasks/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Tasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program app = new Program();
+
+            Task firstTask = new Task(app.FirstTask);
+            firstTask.Start();
+
+            Task secondTask = Task.Factory.StartNew(app.SecondTask);
+
+            app.ThirdTask();
+
+            Thread.CurrentThread.Name = "Main Thread";
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Thread.Sleep(500);
+            }
+
+            firstTask.Wait();
+            secondTask.Wait();
+
+            //Console.ReadKey();
+        }
+
+        public void FirstTask()
+        {
+            Thread.CurrentThread.Name = "First Task";
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public void SecondTask()
+        {
+            Thread.CurrentThread.Name = "Second Task";
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Thread.Sleep(1000);
+            }
+        }
+
+        public async void ThirdTask()
+        {
+            await Task.Run(() =>
+            {
+                Thread.CurrentThread.Name = "Third Task";
+
+                for (int i = 0; i < 15; i++)
+                {
+                    Console.WriteLine(Thread.CurrentThread.Name);
+                    Thread.Sleep(1000);
+                }
+            });
+        }
+    }
+}
